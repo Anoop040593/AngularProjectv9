67ed5f1f6702a6379945a157080f2d790512b938
Anoop Pemmaraju <anuppemmaraju@outlook.com>
2025-11-01 21:56:46 +0530

    Listings page changes

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
-  styleUrls: ['./listings-page.component.css']
+  styleUrls: ['./listings-page.component.css'],
 })
 export class ListingsPageComponent implements OnInit {
-
-  constructor() { }
+  listings: Listing[] = [];
+  constructor() {}
 
   ngOnInit(): void {
+    this.listings = fakeListings;
   }
-
 }
